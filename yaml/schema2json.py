d01045ccd5fb35187d0da58d07adc34219a37ebf
Laura Hughes <flaneuse@users.noreply.github.com>
2020-04-07 13:40:45 -0700

    add analysis.json

---
+++ yaml/schema2json.py
@@ -0,0 +1,20 @@
+# Converts a schema in .yaml format to .json
+# Usage: `python3 schema2json.py analysis.yaml` --> analysis.json
+import sys
+import os.path
+import json
+import yaml
+
+
+def convert(yaml_file, outfile=None):
+  '''Convert input yaml file as JSON file.'''
+  with open(yaml_file) as in_f:
+      schema_data = yaml.safe_load(in_f)
+      outfile = os.path.splitext(yaml_file)[0] + '.jsonld'
+      with open(outfile, 'w') as out_f:
+          json.dump(schema_data, out_f, indent=2)
+
+
+if __name__ == '__main__':
+  yaml_file = sys.argv[1]
+  convert(yaml_file)
